4219bf3a63bdc610383feb9a2d25cc2763e8104d
AlexisNarvaez <alexiszed19@gmail.com>
2019-11-23 22:21:26 -0600

    Grafica dinamica con Sockets y una interfaz para votar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GraficaEncuestaComponent } from './components/grafica-encuesta/grafica-encuesta.component';
 import { ChartsModule } from 'ng2-charts';
+import { FormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
@@ -17,6 +18,7 @@ const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
   imports: [
     BrowserModule,
     ChartsModule,
+    FormsModule,
     HttpClientModule,
     SocketIoModule.forRoot(config)
   ],
--- src/app/components/grafica-encuesta/grafica-encuesta.component.ts
+++ src/app/components/grafica-encuesta/grafica-encuesta.component.ts
@@ -1,41 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { setInterval } from 'timers';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { WebsocketService } from '../../services/websocket.service';
 
 @Component({
-  selector: 'app-grafica-encuesta',
-  templateUrl: './grafica-encuesta.component.html',
-  styleUrls: ['./grafica-encuesta.component.css']
+  selector: "app-grafica-encuesta",
+  templateUrl: "./grafica-encuesta.component.html",
+  styleUrls: ["./grafica-encuesta.component.css"]
 })
 export class GraficaEncuestaComponent implements OnInit {
 
-  public barChartLabels: Array<any> = ['Pregunta 1', 'Pregunta 2', 'Pregunta 3', 'Pregunta 4'];
-  public barChartType: any = 'bar';
+  preguntas = ['1', '2', '3', '4'];
+
+  public barChartLabels: Array<any> = [
+    "Pregunta 1",
+    "Pregunta 2",
+    "Pregunta 3",
+    "Pregunta 4"
+  ];
+  public barChartType: any = "bar";
 
   public barChartData: Array<any> = [
-    { data: [65, 59, 80, 81], label: 'Preguntas' }
+    { data: [0, 0, 0, 0], label: "Preguntas" }
   ];
 
-  constructor() { }
+  constructor(private http: HttpClient, private wsService: WebsocketService) {}
 
   ngOnInit() {
+    this.getData();
+    this.escucharSocket();
+  }
 
+  getData() {
+    this.http.get("http://localhost:5000/encuesta").subscribe((resp: any) => {
+      console.log(resp);
 
-    setInterval( () => {
+      this.barChartData = resp;
+    });
+  }
 
-      const newData = [
-        Math.round( Math.random() * 100),
-        Math.round( Math.random() * 100),
-        Math.round( Math.random() * 100),
-        Math.round( Math.random() * 100),
-      ];
-
-      this.barChartData = [
-        { data: newData, label: 'Preguntas'}
-      ];
-
-    }, 5000);
-
+  escucharSocket() {
+    this.wsService.listen('cambios-encuesta').subscribe( (resp: any) => {
+      this.barChartData = resp;
+      console.log('socket', resp);
+    });
 
   }
+
+  enviarValores(form) {
+
+    console.log(form);
+
+    // tslint:disable-next-line: whitespace
+    // tslint:disable-next-line: no-angle-bracket-type-assertion
+    const questionNumber = (<HTMLInputElement> document.getElementById('pregunta')).value;
+
+    const pregunta = questionNumber;
+    const valor = form.valor;
+
+    const voto = {
+      pregunta,
+      valor
+    };
+
+    this.http.post("http://localhost:5000/encuesta", voto).subscribe( resp => {
+      console.log(resp);
+    });
+
+  }
+
 
 }
